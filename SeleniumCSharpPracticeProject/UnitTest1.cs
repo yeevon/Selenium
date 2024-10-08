b4b51c76f52c7715accebc9b2974b0cb2a56354d
Jose de Lima <delimajm@gmail.com>
2024-10-07 20:49:04 -0500

    Initial C# project commit

---
+++ SeleniumCSharpPracticeProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SeleniumCSharpPracticeProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
